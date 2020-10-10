41dcbced1fec248b8f51e0f9256d5912b5da0c94
lvshuy <lsy-181@qq.com>
2020-10-10 09:15:33 +0800

    新建分支

--- src/main.js
+++ src/main.js
@@ -1,7 +1,26 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Home from './components/Home'
+import Menu from './components/Menu'
+import Admin from './components/Admin'
+import About from './components/about/About'
+import Login from './components/Login'
+import Register from './components/Register'
+
+Vue.use(VueRouter);
+
+const routes = [
+	{path:'/',name:"homeLink",components:Home},
+  {path:'/menu',name:'menuLink',component:Menu},
+  {path:'/admin',name:'adminLink',component:Admin},
+  {path:'/about',name:'aboutLink',component:About
+  {path:'/login',name:'loginLink',component:Login},
+  {path:'/register',name:'registerLink',component:Register},
+  {path:'*',redirect:'/'}
+]
 
 new Vue({
   el: '#app',
+  router,
   render: h => h(App)
 })
